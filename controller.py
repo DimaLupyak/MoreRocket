fe2b51dc76edf1ce6f9985727f7888e5046d2ef4
Vadim Ozarinskiy <worldofvadim@gmail.com>
2018-10-20 20:39:24 +0300

    renamed controller handlers

--- controller.py
+++ controller.py
@@ -1,29 +1,29 @@
 from flask import Flask, jsonify, request
 from handler.database import db_session
 from handler.database import init_db
-import json
+import handler.events as eventHandler
+import handler.user as userHandler
 
 init_db()
 
 app = Flask(__name__)
-import handler.events as handler
 
 
 @app.route("/api/events")
 def getEvent():
-    return jsonify(handler.getEvents())
+    return jsonify(eventHandler.getEvents())
 
 
 @app.route("/api/subscribe")
 def subsribe():
     email = request.args['email']
-    handler.subscribe(email)
+    userHandler.subscribe(email)
     return "OK"
 
 
 @app.route("/api/user")
 def getAllUsers():
-    return jsonify(handler.getAllUsers())
+    return jsonify(userHandler.getAllUsers())
 
 
 @app.teardown_appcontext
