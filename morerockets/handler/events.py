585121f100ff0a0e0e3d9066652704a58b3609e8
Vadim Ozarinskiy <worldofvadim@gmail.com>
2018-10-21 02:09:15 +0300

    getCountry by location

--- morerockets/handler/events.py
+++ morerockets/handler/events.py
@@ -6,3 +6,11 @@ def getEvents():
 
     # insert you event grabber here
     return [e.serialize() for e in eventsList]
+
+
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent='morerockets')
+
+def getCountry(latitude, longitude):
+    return geolocator.reverse(str(latitude) + ", " + str(longitude)).raw['address']['country']
