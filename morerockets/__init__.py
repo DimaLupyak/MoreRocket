4afca00f8f3dd64162b7f7fd4538f7290e6c6e4f
Vadim Ozarinskiy <worldofvadim@gmail.com>
2018-10-21 01:40:41 +0300

    schedule run update

--- morerockets/__init__.py
+++ morerockets/__init__.py
@@ -1,4 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
+
 application = app = Flask(__name__)
 CORS(app)
+
+import morerockets.handler
+
+from morerockets.handler.events import getEvents
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=getEvents, trigger="interval", minutes=60)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
--- morerockets/handler/events.py
+++ morerockets/handler/events.py
@@ -1,8 +1,8 @@
 import morerockets.spacelaunchnow_adapter as sp_adapter
-from morerockets.models import EventHandler
+
 
 def getEvents():
     eventsList = sp_adapter.getItems()
-    
+
     # insert you event grabber here
     return [e.serialize() for e in eventsList]
