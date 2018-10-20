5dfe546efebbc0b597843e7c491cba3b644217f4
Vadim Ozarinskiy <worldofvadim@gmail.com>
2018-10-20 19:00:23 +0300

    Controller example

--- controller.py
+++ controller.py
@@ -1,6 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import jsonify
+import json
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+import main
+
+
+@app.route("/api")
+def getEvent():
+    return jsonify(main.EventHandler("name", "some date", "this is some description", "site some", "some company",
+                             "http://someurl.com").__dict__)
